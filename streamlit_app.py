9ec81b1a0bed3aa93d12e892c937190e4f8653af
emmanuelplan-hsb <138848752+emmanuelplan-hsb@users.noreply.github.com>
2024-03-29 09:07:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,8 @@ Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
 If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
 forums](https://discuss.streamlit.io).
 
-st.write("username:", st.secrets["db_username"])
-st.write("password:", st.secrets["db_password"])
+st.write("username:", st.secrets["username"])
+st.write("password:", st.secrets["password"])
 
 # Example 3
 Example 3
